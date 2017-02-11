4118f97407d0fb955623175f32cb1b3f7a565210
NehemiasEC <neli92santscz@gmail.com>
2017-02-10 19:45:02 -0500

    vamos a probar js and css on server

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	    <title>primer app</title>
 	    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	    
+	    <link rel="stylesheet" href="css/bootstrap.min.css">
+	    <script src="js/jquery-3.1.1.min.js"></script>
 	</head>
 	<body>
 		<div class="container">
@@ -20,6 +21,8 @@
 				print($string);
 				$boolean = true;
 				print($boolean);
+				define("PATH",__FILE__);
+				print(PATH);
 
 		 	?>
 		</div>
