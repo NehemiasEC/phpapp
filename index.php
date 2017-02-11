7acea1b2ad6ef2aa4e7cd84e76a5f338fac3d204
NehemiasEC <neli92santscz@gmail.com>
2017-02-10 19:01:59 -0500

    deleted materialize and added bootstartp

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	    <title>primer app</title>
 	    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+	    
 	</head>
 	<body>
 		<div class="container">
